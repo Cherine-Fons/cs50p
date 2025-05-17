dfacccb4e734ace18cf720e8cd8485932b83d501
Cherine Fons <cherinefons@mac.attlocal.net>
2025-05-17 18:44:52 -0400

    Added variable assignment practice script

---
+++ 02_variables/variable_assignment.py
@@ -0,0 +1,19 @@
+# Variable Assignment Practice
+
+# Assigning values
+name = "Cherine"
+age = 44
+is_math_lover = True
+gpa = 3.83
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Loves Math:", is_math_lover)
+print("Graduate GPA:", gpa)
+
+# Type checking
+print(type(name))
+print(type(age))
+print(type(is_math_lover))
+print(type(gpa))
